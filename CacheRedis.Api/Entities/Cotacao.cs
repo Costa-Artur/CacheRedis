cfd92c0d272eb202baa3b44e6a3ce91cb197f659
Artur <souza.artur@edu.univali.br>
2023-08-02 15:01:08 -0300

    Entity Cotacao

--- CacheRedis.Api/Entities/Cotacao.cs
+++ CacheRedis.Api/Entities/Cotacao.cs
@@ -2,5 +2,9 @@ namespace CacheRedis.Api.Entities;
 
 public class Cotacao 
 {
-    
+    public int CotacaoId {get;set;}
+    public string Sigla {get;set;} = string.Empty;
+    public string NomeMoeda {get;set;} = string.Empty;
+    public string Data {get;set;} = string.Empty;
+    public double Valor {get;set;}
 }
